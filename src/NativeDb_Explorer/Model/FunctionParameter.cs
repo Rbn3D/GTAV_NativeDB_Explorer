e32c9520b012103b86ebe571b0ccbb36eade3889
peach <skpikao233@gmail.com>
2017-12-30 12:40:40 +0000

    Cleanup repo

---
+++ src/NativeDb_Explorer/Model/FunctionParameter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NativeDb_Explorer.Model
+{
+    public class FunctionParameter
+    {
+        public String Type { get; set; }
+        public String Name { get; set; }
+    }
+}
